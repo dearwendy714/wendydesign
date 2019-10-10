38133593ea3e1d96da8db4c01e59e4371a8ad85c
dearwendy <dearwendy714@gmail.com>
2019-10-09 21:52:03 -0700

    Project 1 bug

--- scriptExample.js
+++ scriptExample.js
@@ -1,3 +1,11 @@
-function openProject2(){
-    alert("SDfsdf");
+function openProject(number){
+    document.getElementById('proj-1--button').classList.remove('button-clicked');
+    document.getElementById('proj-2--button').classList.remove('button-clicked');
+    document.getElementById('proj-3--button').classList.remove('button-clicked');   
+    document.getElementById('proj-1--display').classList.remove('proj-show');
+    document.getElementById('proj-2--display').classList.remove('proj-show');
+    document.getElementById('proj-3--display').classList.remove('proj-show');   
+
+    document.getElementById('proj-' + number + '--button').classList.add('button-clicked');   
+    document.getElementById('proj-' + number + '--display').classList.add('proj-show');   
 }
