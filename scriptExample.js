a55d2f25a4aefd4dcbe09f4537b25fd323185d8e
dearwendy <dearwendy714@gmail.com>
2019-10-14 12:20:18 -0700

    update tabs on Projects UXUI

--- scriptExample.js
+++ scriptExample.js
@@ -6,6 +6,11 @@ function openProject(number){
     document.getElementById('proj-2--display').classList.remove('proj-show');
     document.getElementById('proj-3--display').classList.remove('proj-show');   
 
+    // if(document.getElementById('proj-' + number + '--button').classList.contains('button-clicked')  ){
+    //     window.open('/Project' + number + '.html');
+    //     alert('dfg')
+    // }
+
     document.getElementById('proj-' + number + '--button').classList.add('button-clicked');   
     document.getElementById('proj-' + number + '--display').classList.add('proj-show');   
 }
