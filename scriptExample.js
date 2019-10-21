2e44166fcbe056753513671f34aa594f2bbaf854
dearwendy <dearwendy714@gmail.com>
2019-10-20 17:19:16 -0700

    updated button to view projects

--- scriptExample.js
+++ scriptExample.js
@@ -6,11 +6,12 @@ function openProject(number){
     document.getElementById('proj-2--display').classList.remove('proj-show');
     document.getElementById('proj-3--display').classList.remove('proj-show');   
 
-    // if(document.getElementById('proj-' + number + '--button').classList.contains('button-clicked')  ){
-    //     window.open('/Project' + number + '.html');
-    //     alert('dfg')
-    // }
+ 
 
     document.getElementById('proj-' + number + '--button').classList.add('button-clicked');   
     document.getElementById('proj-' + number + '--display').classList.add('proj-show');   
+    //  if(document.getElementById('proj-' + number + '--button').classList.contains('button-clicked')  ){
+    //      window.open('/Project' + number + '.html');
+    //      alert('dfg')
+    //   }
 }
